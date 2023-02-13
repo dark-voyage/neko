da15537f89db2509e3e83e7c83e1fc8aef1fe61b
Yūri Katsuki <yuri@katsuki.moe>
2023-02-13 16:59:13 +0500

    adding routes for server

---
+++ route.c
@@ -0,0 +1,60 @@
+//
+// Created by Sokhibjon Orzikulov on 13/02/23.
+//
+
+#include "route.h"
+#include "file.h"
+#include "http.h"
+
+void route() {
+  ROUTE_START()
+
+  GET("/") {
+    char index_html[20];
+    sprintf(index_html, "%s%s", PUBLIC_DIR, INDEX_HTML);
+
+    HTTP_200;
+    if (file_exists(index_html)) {
+      read_file(index_html);
+    } else {
+      printf("Hello! You are using %s\n\n", request_header("User-Agent"));
+    }
+  }
+
+  GET("/test") {
+    HTTP_200;
+    printf("List of request headers:\n\n");
+
+    header_t *h = request_headers();
+
+    while (h->name) {
+      printf("%s: %s\n", h->name, h->value);
+      h++;
+    }
+  }
+
+  POST("/") {
+    HTTP_201;
+    printf("Wow, seems that you POSTed %d bytes.\n", payload_size);
+    printf("Fetch the data using `payload` variable.\n");
+    if (payload_size > 0)
+      printf("Request body: %s", payload);
+  }
+
+  GET(uri) {
+    char file_name[255];
+    sprintf(file_name, "%s%s", PUBLIC_DIR, uri);
+
+    if (file_exists(file_name)) {
+      HTTP_200;
+      read_file(file_name);
+    } else {
+      HTTP_404;
+      sprintf(file_name, "%s%s", PUBLIC_DIR, NOT_FOUND_HTML);
+      if (file_exists(file_name))
+        read_file(file_name);
+    }
+  }
+
+  ROUTE_END()
+}
---
+++ route.h
@@ -0,0 +1,15 @@
+//
+// Created by Sokhibjon Orzikulov on 13/02/23.
+//
+
+#ifndef NEKO_ROUTE_H
+#define NEKO_ROUTE_H
+
+// Public directory settings
+#define PUBLIC_DIR "./public"
+#define INDEX_HTML "/index.html"
+#define NOT_FOUND_HTML "/404.html"
+
+void route();
+
+#endif//NEKO_ROUTE_H
