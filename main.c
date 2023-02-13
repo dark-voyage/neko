6f13f88447e73f8d6ff02ccddf9febd774107baf
Yūri Katsuki <yuri@katsuki.moe>
2023-02-13 16:59:31 +0500

    adding finishing touches with main headpoint

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
-#include <stdio.h>
+#include "http.h"
 
-int main() {
-    printf("Hello, Neko :D\n");
-    return 0;
+int main(int c, char **v) {
+  char *port = c == 1 ? "8000" : v[1];
+  serve_forever(port);
+  return 0;
 }
