b7835a59caa29f2c6b2e3effe6754fbd518e3bf1
Yūri Katsuki <yuri@katsuki.moe>
2023-02-13 16:58:56 +0500

    creating FILE API

---
+++ file.c
@@ -0,0 +1,41 @@
+//
+// Created by Sokhibjon Orzikulov on 13/02/23.
+//
+
+#include "file.h"
+#include "http.h"
+
+#include <sys/stat.h>
+
+int main(int c, char **v) {
+  char *port = c == 1 ? "8000" : v[1];
+  serve_forever(port);
+  return 0;
+}
+
+int file_exists(const char *file_name) {
+  struct stat buffer;
+  int exists;
+
+  exists = (stat(file_name, &buffer) == 0);
+
+  return exists;
+}
+
+int read_file(const char *file_name) {
+  char buf[CHUNK_SIZE];
+  FILE *file;
+  size_t nread;
+  int err = 1;
+
+  file = fopen(file_name, "r");
+
+  if (file) {
+    while ((nread = fread(buf, 1, sizeof buf, file)) > 0)
+      fwrite(buf, 1, nread, stdout);
+
+    err = ferror(file);
+    fclose(file);
+  }
+  return err;
+}
