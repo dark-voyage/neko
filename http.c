a972fbfaed0f1460bc7a79245b7946d1ed0b64f4
Yūri Katsuki <yuri@katsuki.moe>
2023-02-13 16:01:04 +0500

    implementing responder for requests :D

--- http.c
+++ http.c
@@ -63,7 +63,7 @@ void serve_forever(const char *PORT) {
   start_serve(PORT);
 
   // Ignore child process termination
-  signal(SIGCHILD, SIG_IGN);
+  signal(SIGCHLD, SIG_IGN);
 
   while (1) {
     addrlen = sizeof(clientaddr);
@@ -88,5 +88,74 @@ void serve_forever(const char *PORT) {
   }
 }
 
+void respond(int slot) {
+  int rcvd;
+
+  buf = malloc(BUF_SIZE);
+  rcvd = recv(clients[slot], buf, BUF_SIZE, 0);
+
+  if (rcvd < 0)          // receive error
+    fprintf(stderr, ("recv() error\n"));
+  else if (rcvd == 0)    // receive socket closed
+    fprintf(stderr, "Client disconnected unexpectedly.\n");
+  else                   // message received
+  {
+    buf[rcvd] = '\0';
+
+    method = strtok(buf, " \t\r\n");
+    uri = strtok(NULL, " \t");
+    prot = strtok(NULL, " \t\r\n");
+
+    uri_unescape(uri);
+
+    qs = strchr(uri, '?');
+
+    if (qs)
+      *qs++ = '\0'; // Split uri and qs
+    else
+      qs = uri - 1; // Use empty string
+
+    header_t *h = reqhdr;
+    char *t, *t2;
+    while (h < reqhdr + 16) {
+      char *key, *val;
+
+      key = strtok(NULL, "\r\n: \t");
+      if (!key)
+        break;
+
+      val = strtok(NULL, "\r\n");
+      while (*val && *val == ' ')
+        val++;
+
+      h->name = key;
+      h->value = val;
+      h++;
+      fprintf(stderr, "[H] %s: %s\n", key, val);
+      t = val + 1 + strlen(val);
+      if (t[1] == '\r' && t[2] == '\n')
+        break;
+    }
+
+    t = strtok(NULL, "\r\n");
+    t2 = request_header("Content-Length"); // and the related header if there is
+    payload = t;
+    payload_size = t2 ? atol(t2) : (rcvd - (t - buf));
+
+    int clientfd = clients[slot];
+    dup2(clientfd, STDOUT_FILENO);
+    close(clientfd);
+
+    // Calling the router
+    route();
+
+    // Freeing the buffer
+    fflush(stdout);
+    shutdown(STDOUT_FILENO, SHUT_WR);
+    close(STDOUT_FILENO);
+  }
+
+  free(buf);
+}
 
 
